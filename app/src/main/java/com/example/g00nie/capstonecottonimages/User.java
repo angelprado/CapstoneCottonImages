acb60dd6a6dc8645ac4e32acfe609e63b20eb4a2
Alejandro Giron <alejandro.giron001@mymdc.net>
2017-02-02 18:30:11 -0500

    Updated User Class

--- app/src/main/java/com/example/g00nie/capstonecottonimages/User.java
+++ app/src/main/java/com/example/g00nie/capstonecottonimages/User.java
@@ -6,6 +6,68 @@ package com.example.g00nie.capstonecottonimages;
 
 public class User {
 
+    String UserID,
+            FirstName,
+            lastName,
+            PhoneNumber,
+            EmailAddress,
+            UserName,
+            Password;
 
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        EmailAddress = emailAddress;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 
 }
